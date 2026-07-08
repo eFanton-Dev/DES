778c02cac87afe7e003e543a73df2beb0eb13dae
F-UnKown <edoardo.fanton.07@gmail.com>
2026-07-08 15:42:38 +0200

    Removed .vscode and build folders

--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Programmazione/Misc/DES/tests
-# Build directory: D:/Programmazione/Misc/DES/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("D:/Programmazione/Misc/DES/build/tests/DES_test[1]_include.cmake")
-subdirs("../_deps/googletest-build")
